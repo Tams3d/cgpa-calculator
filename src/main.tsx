ea4ae96c30b72bdb4677c8bb8482c0804bcb6e17
Tamil Selvan <106262964+Tams3d@users.noreply.github.com>
2026-08-16 14:56:35 +0530

    Chore: Add Vercel Analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { Analytics } from "@vercel/analytics/react";
 import "./index.css";
 import App from "./App.tsx";
 import { ThemeProvider } from "./components/theme/theme-provider.tsx";
@@ -8,6 +9,7 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider defaultTheme="system">
       <App />
+      <Analytics />
     </ThemeProvider>
   </StrictMode>,
 );
